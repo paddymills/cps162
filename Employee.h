91db7f446e4798abd2646e02fcd2604730c3dcf9
paddymills <paddymills@pm.me>
2020-08-27 12:19:40 -0400

    Employee breakout

---
+++ Employee.h
@@ -0,0 +1,40 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <string>
+
+class Employee
+{
+private:
+  string name;
+  int idNumber;
+  string department;
+  string position;
+
+public:
+  Employee()
+  {
+    name = "";
+    idNumber = 0;
+    department = "";
+    position = "";
+  }
+  Employee()
+  {
+    name = "";
+    idNumber = 0;
+    department = "";
+    position = "";
+  }
+  Employee();
+  {
+    name = "";
+    idNumber = 0;
+    department = "";
+    position = "";
+  }
+
+  ~Employee();
+};
+
+#endif
