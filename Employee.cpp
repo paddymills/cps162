6dc04e6f0ebe855cfda75597a51e1af4d5573dda
paddymills <paddymills@pm.me>
2020-09-02 08:41:25 -0400

    works as it should

--- Employee.cpp
+++ Employee.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
+#include <memory>
 #include "Employee.h"
 
 using namespace std;
 
 void outputEmployee(Employee);
 
-
-int main() {
-    // create employee1
+int main()
+{
+    // employee 1
     Employee susan("Susan Meyers", 47899, "Accounting", "Vice President");
 
     // employee 2
-    // Employee* mark = new Employee("Mark Jones", 39119);
-    // (&mark).setDepartment("IT");
-    // (&mark).setPosition("Programmer");
+    Employee *mark = new Employee("Mark Jones", 39119);
+    mark->setDepartment("IT");
+    mark->setPosition("Programmer");
 
-    // unique_ptr<Employee> joy(new Employee());
-    // joy.setName("Joy Rogers");
-    // joy.setIdNumber(81774);
-    // joy.setDepartment("Manufacturing");
-    // joy.setPosition("Engineer");
-   
+    // employee 3
+    unique_ptr<Employee> joy(new Employee());
+    joy->setName("Joy Rogers");
+    joy->setIdNumber(81774);
+    joy->setDepartment("Manufacturing");
+    joy->setPosition("Engineer");
 
     // output employees
     outputEmployee(susan);
-    // outputEmployee(*mark);
-    
+    outputEmployee(*mark);
+    outputEmployee(*joy);
 
     return 0;
 }
 
-void outputEmployee(Employee emp) {
+void outputEmployee(Employee emp)
+{
     cout << "Employee Name: " << emp.getName() << endl;
     cout << "Employee Id: " << emp.getIdNumber() << endl;
     cout << "Employee Department: " << emp.getDepartment() << endl;
