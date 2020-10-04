5e9fee0de187bb337b663713b60426b50c65e7db
Patrick Miller <paddymills@pm.me>
2020-10-04 12:56:04 -0400

    add iomanip

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "LinkedList.h"
 #include <iostream>
+#include <iomanip>
 #include <list>
 
 using namespace std;
@@ -20,8 +21,7 @@ void test_our_list()
   LinkedList list;
 
   // append items
-  list.displayNodes();
-  cout << "Appending nodes 2, 5, 8, 15" << setw(20) << "-> ";
+  cout << "Appending nodes 2, 5, 8, 15" << setw(30) << "-> ";
   list.appendNode(2);
   list.appendNode(5);
   list.appendNode(8);
@@ -29,24 +29,24 @@ void test_our_list()
   list.displayNodes();
 
   // insert items
-  cout << "Insert 3 at index 1" << setw(20) << "-> ";
+  cout << "Insert 3 at index 1" << setw(30) << "-> ";
   list.insertNode(3, 1);
   list.displayNodes();
-  cout << "Insert 7 at index 3" << setw(20) << "-> ";
+  cout << "Insert 7 at index 3" << setw(30) << "-> ";
   list.insertNode(7, 3);
   list.displayNodes();
 
   // delete items
-  cout << "Deleting index 2" << setw(20) << "-> ";
+  cout << "Deleting index 2" << setw(30) << "-> ";
   list.deleteNode(2);
   list.displayNodes();
-  cout << "Deleting index 4" << setw(20) << "-> ";
+  cout << "Deleting index 4" << setw(30) << "-> ";
   list.deleteNode(4);
   list.displayNodes();
-  cout << "Deleting index 1" << setw(20) << "-> ";
+  cout << "Deleting index 1" << setw(30) << "-> ";
   list.deleteNode(1);
   list.displayNodes();
-  cout << "Deleting index 0" << setw(20) << "-> ";
+  cout << "Deleting index 0" << setw(30) << "-> ";
   list.deleteNode(0);
   list.displayNodes();
 }
