024d98287a262763d9058a22bfe48be8193af3ad
Patrick Miller <paddymills@pm.me>
2020-10-04 15:19:21 -0400

    prettier output

--- main.cpp
+++ main.cpp
@@ -20,8 +20,11 @@ void test_our_list()
 {
   LinkedList list;
 
+  cout << left;
+
   // append items
-  cout << "Appending nodes 2, 5, 8, 15" << setw(30) << "-> ";
+  cout << setw(30) << "Appending nodes 2, 5, 8, 15"
+       << "-> ";
   list.appendNode(2);
   list.appendNode(5);
   list.appendNode(8);
@@ -29,24 +32,30 @@ void test_our_list()
   list.displayNodes();
 
   // insert items
-  cout << "Insert 3 at index 1" << setw(30) << "-> ";
+  cout << setw(30) << "Insert 3 at index 1"
+       << "-> ";
   list.insertNode(3, 1);
   list.displayNodes();
-  cout << "Insert 7 at index 3" << setw(30) << "-> ";
+  cout << setw(30) << "Insert 7 at index 3"
+       << "-> ";
   list.insertNode(7, 3);
   list.displayNodes();
 
   // delete items
-  cout << "Deleting index 2" << setw(30) << "-> ";
+  cout << setw(30) << "Deleting index 2"
+       << "-> ";
   list.deleteNode(2);
   list.displayNodes();
-  cout << "Deleting index 4" << setw(30) << "-> ";
+  cout << setw(30) << "Deleting index 4"
+       << "-> ";
   list.deleteNode(4);
   list.displayNodes();
-  cout << "Deleting index 1" << setw(30) << "-> ";
+  cout << setw(30) << "Deleting index 1"
+       << "-> ";
   list.deleteNode(1);
   list.displayNodes();
-  cout << "Deleting index 0" << setw(30) << "-> ";
+  cout << setw(30) << "Deleting index 0"
+       << "-> ";
   list.deleteNode(0);
   list.displayNodes();
 }
