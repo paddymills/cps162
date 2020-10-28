a0e2d312bc83b496357adc099b823fc6674592b8
Patrick Miller <paddymills@pm.me>
2020-10-28 18:33:18 -0400

    lab done

--- lab/ch10NyhoffLab.cpp
+++ lab/ch10NyhoffLab.cpp
@@ -10,8 +10,8 @@ int main()
 {
   string test("I lOVe PrOgraMMiNg in C++");
 
-  string res = findCapsRec(test);
-  cout << "capitals: " << res << endl;
+  cout << "capitals (rec): " << findCapsRec(test) << endl;
+  cout << "capitals (non-rec): " << findCapsNonRec(test) << endl;
 
   return 0;
 }
@@ -20,17 +20,30 @@ string findCapsRec(string s)
 {
   string result;
 
-  // get first character
-  char c = s.front();
-
   // ascii range for uppercase is 65 - 90 (inclusive)
-  // if out of range, set to null
-  if (c < 65 || c > 90)
-    c = 0;
+  if (s[0] >= 65 && s[0] <= 90)
+    result.push_back(s[0]);
 
-  result.push_back(c);
+  // recursive call if not at end of string
   if (s.length() > 1)
     result.append(findCapsRec(s.substr(1, s.length() - 1)));
+
+  return result;
+}
+
+string findCapsNonRec(string s)
+{
+  string result; // return string
+  char c;        // current character
+
+  for (int i = 0; i < s.length(); i++)
+  {
+    char c = s[i];
+
+    // ascii range for uppercase is 65 - 90 (inclusive)
+    if (c >= 65 && c <= 90)
+      result.push_back(c);
+  }
 
   return result;
 }
